9609b4a264aeb2274916b0e242bcc62dd90ad814
minhazapon <151423201+minhazapon@users.noreply.github.com>
2024-09-16 03:16:44 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const express = require('express')
 const cors = require('cors')
-const phones = require('./phones.json')
-const app = express() 
-const port = 5000
+const data = require('./data.json')
+const app = express()
+const port = 3000
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Is this apon server')
+  res.send('this is minhazul abedin apon server ')
 })
 
-
-app.get('/phones', (req, res) => {
-  res.send(phones)
+app.get('/data', (req, res) => {
+  res.send(data)
 })
 
-app.get('/phones/:id', (req, res) => {
+app.get('/data/:id', (req, res) => {
   
-  const id = parseInt(req.params.id);
-  console.log('this is phone data ------->', id)
-  const phone = phones.find(phone => phone.id === id ) || {};
-  res.send(phone)
+    const id = parseInt( req.params.id)
+    console.log( 'this out data', id)
+    const dataS = data.find(data => data.id === id ) || {};
+    res.send(dataS)
 
-})
+
+}) 
 
 app.listen(port, () => {
-  console.log(`Ports-----------> ${port}`)
-})
+  console.log(`apon Port-----------> ${port}`)
+}) 
+
